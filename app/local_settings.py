67fee7d7bba34c18512e89e90420c163b7aee47a
Umutbek <karimovumutbek@gmail.com>
2021-11-21 20:35:39 +0000

    Merge problem fix

--- app/local_settings.py
+++ app/local_settings.py
@@ -1,6 +1,6 @@
 SECRET_KEY = 'django-insecure-y_@@zrr4pc2e5#9v*9=$wkq5dwvf88876^&(xefkwlh@5wi*&*ts'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['138.68.99.168']
 
